51b9f20fb6694c3d627d0fe96be2837c141d1dc8
param0205 <param020599@gmail.com>
2024-12-27 19:57:24 +0530

    footer changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
     <div className="bg-rose-500 h-[200px] flex justify-center">
       <div>
         <SocialMediaIcons />
-        <p><span><i class="material-icons"></i>
-        </span>Electronic City,Bangalore</p>
+        <p className="font-playfiar font-semibold text-1xl"><span><i class="material-icons"></i>
+        </span>Electronic City, Bangalore</p>
       </div>
     </div>
   );
